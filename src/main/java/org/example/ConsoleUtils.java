ed1c53c73d46db3eee037cf926a6be8e2924d7f2
Chester47 <vazilin71@gmail.com>
2023-10-03 01:14:44 +0300

    [peredelal']

--- src/main/java/org/example/ConsoleUtils.java
+++ src/main/java/org/example/ConsoleUtils.java
@@ -2,14 +2,20 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class ConsoleUtils {
+    public void getPlayerNickname() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите ник игрока: ");
+    }
+    public void getPlayerLogin() {
+        Scanner scanner = null;
         String nickname = scanner.nextLine();
         System.out.print("Введите логин игрока: ");
-        String login = scanner.nextLine();
+    }
+    public void getPlayerPassword() {
+        String nickname = null;
         System.out.println("Ник игрока: " + nickname);
+        String login = null;
         System.out.println("Логин игрока: " + login);
     }
 }
