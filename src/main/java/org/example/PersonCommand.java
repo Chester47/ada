9a95d89e47479bdb94866e6c35281e1643b81604
Chester47 <vazilin71@gmail.com>
2023-10-09 04:58:09 +0300

    vrod' tak

---
+++ src/main/java/org/example/PersonCommand.java
@@ -0,0 +1,13 @@
+package org.example;
+
+    public class PersonCommand implements Command {
+        @Override
+        public void apply() {
+
+        }
+
+        @Override
+        public String getInformation() {
+            return null;
+        }
+    }
