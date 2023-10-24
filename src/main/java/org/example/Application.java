cbf1637efbc49ef4d631d84b88d4498b47a13f41
Chester47 <vazilin71@gmail.com>
2023-10-24 17:41:52 +0300

    ахуевший чам

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,12 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        Dispatcher dispatcher = new Dispatcher();
-        do {
-            dispatcher.invoke();
-        } while (true);
+        SpringApplication.run(Application.class, args);
     }
 }
-
-
